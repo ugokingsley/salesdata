61ad6de0d4e729348366ac14d70613b5a2b3479a
ugokingsley <ugokingsley5@gmail.com>
2019-10-27 22:15:34 +0100

    initials

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,3 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, "static", "static-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
